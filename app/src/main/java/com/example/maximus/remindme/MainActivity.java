cab6d3cb92fc9f04ba4dcc834d7dfabfafb75636
Maxim Lagutkin <maxim.lagutkin@gmail.com>
2017-10-10 15:40:16 +0300

    Added MainActivity to project

--- app/src/main/java/com/example/maximus/remindme/MainActivity.java
+++ app/src/main/java/com/example/maximus/remindme/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
